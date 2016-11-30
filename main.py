a639892ef5fae4da5bfe4eebadfa9932227043ab
Beaver Worx <beaverworks2609@gmail.com>
2016-11-30 15:17:30 -0500

    added networktable hsv sliders read/write

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import cv2
 import numpy
 from networktables import NetworkTable
 
-NetworkTable.setIPAddress("roborio-2609-frc.local")#Change the address to your own
+NetworkTable.setIPAddress("10.26.9.12")#Change the address to your own
 NetworkTable.setClientMode()
 NetworkTable.initialize()
+sd = NetworkTable.getTable("RaspberryPi")
 
 # NT publish number (fps)
 # //Resize to 320x240 Cubic
@@ -55,67 +56,92 @@ time.sleep(0.1)
 loops = 0
 timesum = 0 
 CPS = 0
-sd = NetworkTable.getTable("SmartDashboard")
+sd.putNumber('H_L',40)
+sd.putNumber('H_U',70)
+sd.putNumber('S_L',120)
+sd.putNumber('S_U',255)
+sd.putNumber('V_L',120)
+sd.putNumber('V_U',255)
+
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	timestart = time.time()
-	image = frame.array
+        
+        
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    timestart = time.time()
+    image = frame.array
 
-        # CV erode iterations 1 Border_constant
-	image_erosion = cv2.erode(image, kernel, iterations=1)
+    # CV erode iterations 1 Border_constant
+    #image_erosion = cv2.erode(image, kernel, iterations=1)
 
-        # CV dilate iterations 2 Border_constant
-	image_dilation = cv2.dilate(image_erosion, kernel, iterations=2)
-	
-        # HSV H:49-97 S: 179-255 V: 25-220
-        lower_green = numpy.array([40,120,120])
-        upper_green = numpy.array([70,255,255])
-        hsv = cv2.cvtColor(image_dilation, cv2.COLOR_BGR2HSV)
-        image_hsv = cv2.inRange(hsv, lower_green, upper_green)
+    # CV dilate iterations 2 Border_constant
+    image_dilation = cv2.dilate(image, kernel, iterations=2)
+    
+    # HSV H:49-97 S: 179-255 V: 25-220
+    H_L = sd.getNumber('H_L')
+    try:
+        H_L = sd.getNumber('H_L')
+        H_U = sd.getNumber('H_U')
+        S_L = sd.getNumber('S_L')
+        S_U = sd.getNumber('S_U')
+        V_L = sd.getNumber('V_L')
+        V_U = sd.getNumber('V_U')
+    except KeyError:
+        print('RaspberryPi Connect: N/A 1')
+    
+    lower_green = numpy.array([H_L,S_L,V_L])
+    upper_green = numpy.array([H_U,S_U,V_U])
+    hsv = cv2.cvtColor(image_dilation, cv2.COLOR_BGR2HSV)
+    image_hsv = cv2.inRange(hsv, lower_green, upper_green)
 
-        # Find contours
-        ret,thresh = cv2.threshold(image_hsv.copy(),127,255,0)
-        im2, contours, hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # Find contours
+    ret,thresh = cv2.threshold(image_hsv.copy(),127,255,0)
+    im2, contours, hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-        # Find bounding box's
-        ret,thresh = cv2.threshold(image_hsv.copy(),127,255,0)
-        test1,contours,hierarchy = cv2.findContours(thresh.copy(), 1, 2)
-        try:
-                cnt = contours[0]
-                x,y,w,h = cv2.boundingRect(cnt)
-                cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-                sd.putNumber('centerX', (x+w/2))
-                sd.putNumber('centerY', (y+h/2))
-        except IndexError:
-                print ("Index error")
-        # show the frame and other images
-	
-        cv2.drawContours(image, contours, -1, (0,255,0), 3)
-	cv2.putText(image, "CPS: " + str(CPS) + " Loops: " + str(loops), (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
-	cv2.imshow("Frame", image)
-	#cv2.imshow("image_erosion", image_erosion)
-	#cv2.imshow("image_dilation", image_dilation)
-	cv2.imshow("image_hsv", image_hsv)
- 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-        key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-	deltatime = ((time.time()-timestart)*1000)
-	timesum += deltatime
-	loops += 1
-	CPS = timesum/loops
+    # Find bounding box's
+    ret,thresh = cv2.threshold(image_hsv.copy(),127,255,0)
+    test1,contours,hierarchy = cv2.findContours(thresh.copy(), 1, 2)
+    try:
+        cnt = contours[0]
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        sd.putNumber('centerX', (x+w/2))
+        sd.putNumber('centerY', (y+h/2))
+    except IndexError:
+        print ("Index error")
+    # show the frame and other images
+    
+    #cv2.drawContours(image, contours, -1, (0,255,0), 3)
+    #cv2.putText(image, "CPS: " + str(CPS) + " Loops: " + str(loops), (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+    #cv2.imshow("Frame", image)
+    #cv2.imshow("image_erosion", image_erosion)
+    #cv2.imshow("image_dilation", image_dilation)
+    #cv2.imshow("image_hsv", image_hsv)
 
-	
-        try:
-                print('robotTime:', sd.getNumber('robotTime'))
-        except KeyError:
-                print('robotTime: N/A')
-        sd.putNumber('piLoops', loops)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+            break
+    deltatime = ((time.time()-timestart)*1000)
+    timesum += deltatime
+    loops += 1
+    CPS = timesum/loops
+
+    
+##    try:
+##        #print('raspberryTime:', sd.getNumber('test'))
+##        print('H_L:', sd.getNumber('H_L'))
+##        print('H_U:', sd.getNumber('H_U'))
+##        print('S_L:', sd.getNumber('S_L'))
+##        print('S_U:', sd.getNumber('S_U'))
+##        print('V_L:', sd.getNumber('V_L'))
+##        print('V_U:', sd.getNumber('V_U'))
+##    except KeyError:
+##        print('RaspberryPi Connect: N/A 2')
+    sd.putNumber('piLoops', loops)
 
 
 cv2.destroyAllWindows()
