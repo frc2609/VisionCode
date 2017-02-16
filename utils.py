be2186e01d23f47f1e79d7b74893c8d0c31a41e3
beaverworks2609 <beaverworks2609@gmail.com>
2017-02-16 05:58:26 -0500

    added function to retrieve request from smartdashboard about which display indicators to enable

--- utils.py
+++ utils.py
@@ -33,6 +33,11 @@ def targetWrite(target,centerX,centerY,angleToTarget,loops, distanceToTarget):#R
     sd.putNumber('distanceToTarget', distanceToTarget)
     sd.putNumber('piLoops', loops)
     
-def filterRead():
-    contourAreaMin = sd.getNumber('contourAreaMin')
-##    squareRatioMin, squareRationMax, solidityMin, targetHeightMin
+def filterRead(contourAreaMin,squareRatioMin,squareRationMax,solidityMin,targetHeightMin):
+    sd.getNumber('contourAreaMin', 0)
+    sd.getNumber('squareRatioMin', 0)
+    sd.getNumber('squareRationMax', 0)
+    sd.getNumber('solidityMin', 0)
+    sd.getNumber('targetHeightMin', 0)  
+    return(contourAreaMin,squareRatioMin,squareRationMax,solidityMin,targetHeightMin)
+##    contourAreaMin,squareRatioMin, squareRationMax, solidityMin, targetHeightMin
